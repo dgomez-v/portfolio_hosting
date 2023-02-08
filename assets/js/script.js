764eebd4199dd92b6bf90384f81f014b1dd619ce
dgomez-v <dav.dgv.11@gmail.com>
2023-02-08 13:25:28 +0100

    Todo el esqueleto de la web responsive

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// función scroll arriba
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+	window.scrollTo({
+		top: 0,
+		left: 0,
+		behavior: "smooth",
+	});
+});
